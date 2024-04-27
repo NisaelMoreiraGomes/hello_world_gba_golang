5c02b9d239657290f8daa88c4f3a8aecf746ba15
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2024-04-27 11:30:34 -0300

    Meu primeiro retangulo, isso é sensacional!!!

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"machine"
+
+	"github.com/NisaelMoreiraGomes/hello_world_gba_golang/internal/fill"
+)
 
 func main() {
-	fmt.Println("Olá, mundo!!!")
+	machine.Display.Configure()
+
+	var screenWidth, screenHeight = machine.Display.Size()
+
+	var rectW, rectH int16 = 10, 10
+	var rectX, rectY = screenWidth/2 - rectW, screenHeight/2 - rectH
+
+	fill.Rect(rectX, rectY, rectW, rectH, fill.Blue)
 }
