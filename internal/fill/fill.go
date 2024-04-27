3fd487d738c35acdd23f2d078bbcff5ded0c4e58
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2024-04-27 11:24:45 -0300

    Ops!!!

--- internal/fill/fill.go
+++ internal/fill/fill.go
@@ -8,7 +8,7 @@ import (
 func Rect(x, y, w, h int16, c color.RGBA) {
 	for i := int16(0); i < w; i++ {
 		for j := int16(0); j < h; j++ {
-			machine.Display.SetPixel(i+w, j+h, c)
+			machine.Display.SetPixel(i+x, j+y, c)
 		}
 	}
 }
