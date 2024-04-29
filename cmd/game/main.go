34b31443914211ff1e86bf26a1986f9e750afd1f
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2024-04-29 14:55:24 -0300

    Tô muito feliz que está se movendo, mas ta muito bugado e eu não sei o porque.

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -3,16 +3,64 @@ package main
 import (
 	"machine"
 
+	"github.com/NisaelMoreiraGomes/hello_world_gba_golang/internal/actor"
 	"github.com/NisaelMoreiraGomes/hello_world_gba_golang/internal/fill"
+	"github.com/NisaelMoreiraGomes/hello_world_gba_golang/internal/pads"
+	"github.com/NisaelMoreiraGomes/hello_world_gba_golang/internal/timing"
 )
+
+var player = actor.Square{0, 0, 20, fill.Blue}
+var shadow = actor.Square{0, 0, 20, fill.Black}
+var moved = true
 
 func main() {
 	machine.Display.Configure()
 
-	var screenWidth, screenHeight = machine.Display.Size()
+	screenWidth, screenHeight := machine.Display.Size()
+	centerX, centerY := screenWidth/2-20, screenHeight/2-20
 
-	var rectW, rectH int16 = 10, 10
-	var rectX, rectY = screenWidth/2 - rectW, screenHeight/2 - rectH
+	player.X = centerX
+	player.Y = centerY
 
-	fill.Rect(rectX, rectY, rectW, rectH, fill.Blue)
+	shadow.X = centerX
+	shadow.Y = centerY
+
+	fill.Rect(0, 0, screenWidth, screenHeight, fill.Black)
+
+	for {
+		timing.WaitForVBlank()
+		update()
+		draw()
+	}
+}
+
+func update() {
+	key := pads.ReadPad()
+
+	switch {
+	case pads.Up.IsPressed(key):
+		shadow.Y = player.Y
+		player.Y -= 1
+		moved = true
+	case pads.Down.IsPressed(key):
+		shadow.Y = player.Y
+		player.Y += 1
+		moved = true
+	case pads.Left.IsPressed(key):
+		shadow.X = player.X
+		player.X -= 1
+		moved = true
+	case pads.Right.IsPressed(key):
+		shadow.X = player.X
+		player.X += 1
+		moved = true
+	}
+}
+
+func draw() {
+	if moved {
+		shadow.Draw()
+		player.Draw()
+		moved = false
+	}
 }
