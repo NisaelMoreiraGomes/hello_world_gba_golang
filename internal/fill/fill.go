f6fc2760099f39397fed08570480b74a69cfa689
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2024-05-17 18:10:58 -0300

    Por isso que eu amo programação, um pequeno detalhe faz muita diferença!!!

--- internal/fill/fill.go
+++ internal/fill/fill.go
@@ -6,9 +6,14 @@ import (
 )
 
 func Rect(x, y, w, h int16, c color.RGBA) {
-	for i := int16(0); i < w; i++ {
-		for j := int16(0); j < h; j++ {
-			machine.Display.SetPixel(i+x, j+y, c)
+	// https://www.youtube.com/watch?v=G4aHKNUBAgo
+	// Sensacional a explicação de percorrer linha ao invés de coluna.
+	// Resolvi deixar aqui compartilhado.
+	// Bom para eu não esquecer também.
+	// Finalmente eu pude entender porque no projeto do TinyGBA eles fazem assim.
+	for i := int16(0); i < h; i++ {
+		for j := int16(0); j < w; j++ {
+			machine.Display.SetPixel(j+x, i+y, c)
 		}
 	}
 }
